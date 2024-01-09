6727c8c4a21055e4012b52c89c87a9098378023b
FarewellTears <farewelltears97@gmail.com>
2024-01-09 15:26:09 +0800

    :tada: 项目初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+本库为《JavaScript Everywhere》样例 web 代码实操
+
+JavaScript Everywhere 源库指路 [JavaScript Everywhere - Web](https://github.com/javascripteverywhere/web)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  const name = "Adam";
+  const now = String(new Date());
+  return (
+    <div className="App">
+      <p>Hello {name}</p>
+      <p>The current time is {now}</p>
+      <p>Two plus two is {2 + 2}</p>
+    </div>
+  );
+}
+
+export default App;
